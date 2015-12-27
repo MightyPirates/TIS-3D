921943b7e3fda3cf81834f94c559979f18727113
Florian Nücke <florian@nuecke.de>
2015-12-27 12:40:21 +0100

    Organize imports!

--- src/main/java/li/cil/tis3d/client/network/handler/MessageHandlerHaltAndCatchFire.java
+++ src/main/java/li/cil/tis3d/client/network/handler/MessageHandlerHaltAndCatchFire.java
@@ -1,9 +1,7 @@
 package li.cil.tis3d.client.network.handler;
 
 import li.cil.tis3d.common.network.handler.AbstractMessageHandlerWithLocation;
-import li.cil.tis3d.common.network.message.MessageCasingState;
 import li.cil.tis3d.common.network.message.MessageHaltAndCatchFire;
-import li.cil.tis3d.common.tile.TileEntityCasing;
 import li.cil.tis3d.common.tile.TileEntityController;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
