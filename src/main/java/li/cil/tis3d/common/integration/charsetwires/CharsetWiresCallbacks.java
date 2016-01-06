710ec97e5116befbc8184c1ec9a764322d99b24a
Florian Nücke <florian@nuecke.de>
2016-01-06 15:11:12 +0100

    Revert "Updated to Charset 0.1.0 API (for MCMP support)."
    This reverts commit 0e96b4a428604cf6b9da4aae2b75d4db1a78f16c.

--- src/main/java/li/cil/tis3d/common/integration/charsetwires/CharsetWiresCallbacks.java
+++ src/main/java/li/cil/tis3d/common/integration/charsetwires/CharsetWiresCallbacks.java
@@ -7,7 +7,6 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
-import pl.asie.charset.api.lib.CharsetHelper;
 import pl.asie.charset.api.wires.IBundledEmitter;
 import pl.asie.charset.api.wires.IBundledUpdatable;
 import pl.asie.charset.api.wires.IConnectable;
@@ -19,15 +18,15 @@ import java.util.Arrays;
 
 public final class CharsetWiresCallbacks {
     public static void onBundledOutputChanged(final BundledRedstone module, final int channel) {
-        final EnumFacing facing = Face.toEnumFacing(module.getFace());
         final World world = module.getCasing().getCasingWorld();
-        final BlockPos neighborPos = module.getCasing().getPosition().offset(facing);
+        final BlockPos neighborPos = module.getCasing().getPosition().offset(Face.toEnumFacing(module.getFace()));
         if (world.isBlockLoaded(neighborPos)) {
-            for (final EnumFacing face : WIRE_FACES) {
-                final IBundledUpdatable updatable = CharsetHelper.instance.getInterface(IBundledUpdatable.class, world, neighborPos, facing.getOpposite(), face);
-                if (updatable != null) {
-                    updatable.onBundledInputChanged(Face.toEnumFacing(module.getFace().getOpposite()));
-                }
+            final TileEntity tileEntity = world.getTileEntity(neighborPos);
+
+            if (tileEntity instanceof IBundledUpdatable) {
+                final IBundledUpdatable updatable = (IBundledUpdatable) tileEntity;
+
+                updatable.onBundledInputChanged(Face.toEnumFacing(module.getFace().getOpposite()));
             }
         }
     }
@@ -70,33 +69,41 @@ public final class CharsetWiresCallbacks {
         return 0;
     }
 
-    private static final EnumFacing[] WIRE_FACES = new EnumFacing[]{
-            EnumFacing.DOWN,
-            EnumFacing.UP,
-            EnumFacing.NORTH,
-            EnumFacing.SOUTH,
-            EnumFacing.WEST,
-            EnumFacing.EAST,
-            null
-    };
-
     public static int getBundledInput(final BundledRedstone module, final int channel) {
         final EnumFacing facing = Face.toEnumFacing(module.getFace());
         final World world = module.getCasing().getCasingWorld();
         final BlockPos inputPos = module.getCasing().getPosition().offset(facing);
         if (world.isBlockLoaded(inputPos)) {
-            for (final EnumFacing face : WIRE_FACES) {
-                final IBundledEmitter emitter = CharsetHelper.instance.getInterface(IBundledEmitter.class, world, inputPos, facing.getOpposite(), face);
-                if (emitter != null) {
-                    int maxSignal = 0;
-                    final byte[] signal = emitter.getBundledSignal(WireFace.get(face), facing.getOpposite());
+            final TileEntity tileEntity = world.getTileEntity(inputPos);
+
+            final boolean[] connectivity = new boolean[WireFace.VALUES.length];
+            if (tileEntity instanceof IConnectable) {
+                final IConnectable connectable = (IConnectable) tileEntity;
+                for (final WireFace face : WireFace.VALUES) {
+                    connectivity[face.ordinal()] = connectable.canConnect(WireType.BUNDLED, face, facing.getOpposite())
+                            || connectable.canConnect(WireType.INSULATED, face, facing.getOpposite());
+                }
+            } else {
+                Arrays.fill(connectivity, true);
+            }
+
+            if (tileEntity instanceof IBundledEmitter) {
+                final IBundledEmitter emitter = (IBundledEmitter) tileEntity;
+
+                int maxSignal = 0;
+                for (final WireFace face : WireFace.VALUES) {
+                    if (!connectivity[face.ordinal()]) {
+                        continue;
+                    }
+
+                    final byte[] signal = emitter.getBundledSignal(face, facing.getOpposite());
                     if (signal != null) {
                         if ((signal[channel] & 0xFF) > maxSignal) {
                             maxSignal = signal[channel] & 0xFF;
                         }
                     }
-                    return maxSignal;
                 }
+                return maxSignal;
             }
         }
 
