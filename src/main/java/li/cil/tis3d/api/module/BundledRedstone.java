820d98dde11974330ce8f7e0395fa8a6e91118fe
Florian Nücke <florian@nuecke.de>
2016-05-23 20:47:03 +0200

    Removed deprecated interfaces.

--- src/main/java/li/cil/tis3d/api/module/BundledRedstone.java
+++
@@ -1,8 +0,0 @@
-package li.cil.tis3d.api.module;
-
-/**
- * @deprecated Moved to <tt>li.cil.tis3d.api.module.traits.BundledRedstone</tt>.
- */
-@Deprecated
-public interface BundledRedstone extends li.cil.tis3d.api.module.traits.BundledRedstone {
-}
