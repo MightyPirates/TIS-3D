e6f2abcccd2fc6a8ed47289c633647bd0cbc7491
Florian Nücke <florian@nuecke.de>
2017-09-19 00:27:57 +0200

    Fixed DIV 0 crashing, HCF-ing instead. Fixes #70.

--- src/main/java/li/cil/tis3d/common/module/execution/instruction/InstructionDiv.java
+++ src/main/java/li/cil/tis3d/common/module/execution/instruction/InstructionDiv.java
@@ -1,5 +1,6 @@
 package li.cil.tis3d.common.module.execution.instruction;
 
+import li.cil.tis3d.api.machine.HaltAndCatchFireException;
 import li.cil.tis3d.common.module.execution.Machine;
 import li.cil.tis3d.common.module.execution.MachineState;
 import li.cil.tis3d.common.module.execution.target.Target;
@@ -11,6 +12,10 @@ public final class InstructionDiv extends AbstractInstructionRead {
 
     @Override
     protected void doStep(final Machine machine, final int value) {
+        if (value == 0) {
+            throw new HaltAndCatchFireException();
+        }
+
         final MachineState state = machine.getState();
         state.acc = (short) Math.max(Short.MIN_VALUE, Math.min(Short.MAX_VALUE, state.acc / value));
         state.pc++;
