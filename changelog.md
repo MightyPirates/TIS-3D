f4534ace28236467304055f2ee006ac260f9596b
Florian Nücke <florian@nuecke.de>
2018-07-20 23:41:58 +0200

    Added two new instructions, `RRLAST` and `RLLAST`.
    (cherry picked from commit 6e3b483)

--- changelog.md
+++ changelog.md
@@ -1,1 +1,2 @@
 * Added support for defines. Use `#define A B` to create or replace a define, use `#undef A` to remove a define. Defined names can be used where targets or number literals are expected.
+* Added two new instructions, `RRLAST` and `RLLAST`, used to rotate the value of `LAST` one to the right or the left, respectively, if its value is not `NIL`.
---
+++ src/main/java/li/cil/tis3d/common/module/execution/instruction/InstructionLastRotateRight.java
@@ -0,0 +1,18 @@
+package li.cil.tis3d.common.module.execution.instruction;
+
+import li.cil.tis3d.common.module.execution.Machine;
+import li.cil.tis3d.common.module.execution.MachineState;
+
+public final class InstructionLastRotateRight implements Instruction {
+    @Override
+    public void step(Machine machine) {
+        final MachineState state = machine.getState();
+        state.last = state.last.map(p -> p.rotated(1));
+        state.pc++;
+    }
+
+    @Override
+    public String toString() {
+        return "RRLAST";
+    }
+}
