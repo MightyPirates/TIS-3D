ba06dc87d9b6e0eaf60fa3a2ada3667456c57484
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-04-18 15:21:21 +0200

    Register InvisibleEntityRenderer for InfraredPacketEntity
    Game doesn't crash at startup anymore.

--- src/main/java/li/cil/tis3d/common/init/Entities.java
+++ src/main/java/li/cil/tis3d/common/init/Entities.java
@@ -1,8 +1,10 @@
 package li.cil.tis3d.common.init;
 
+import li.cil.tis3d.client.render.entity.InvisibleEntityRenderer;
 import li.cil.tis3d.common.Constants;
 import li.cil.tis3d.common.entity.InfraredPacketEntity;
 import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.minecraft.entity.EntityCategory;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
@@ -18,6 +20,9 @@ public final class Entities {
 
     static void registerEntities() {
         Registry.register(Registry.ENTITY_TYPE, Constants.NAME_ENTITY_INFRARED_PACKET, INFRARED_PACKET);
+
+        EntityRendererRegistry.INSTANCE.register(INFRARED_PACKET,
+            (dispatcher, context) -> new InvisibleEntityRenderer<InfraredPacketEntity>(dispatcher));
     }
 
     // --------------------------------------------------------------------- //
