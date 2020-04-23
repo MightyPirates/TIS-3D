7becb1d278e18df04b420b9d6a8cf6b3aa49cc56
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-04-23 22:12:12 +0200

    Manual: Fix some more texture calls

--- src/main/java/li/cil/tis3d/client/manual/segment/render/TextureImageRenderer.java
+++ src/main/java/li/cil/tis3d/client/manual/segment/render/TextureImageRenderer.java
@@ -1,13 +1,12 @@
 package li.cil.tis3d.client.manual.segment.render;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-//import com.mojang.blaze3d.platform.TextureUtil;
 import li.cil.tis3d.api.manual.ImageRenderer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.texture.AbstractTexture;
 import net.minecraft.client.texture.NativeImage;
-//import net.minecraft.client.texture.Texture;
 import net.minecraft.client.texture.TextureManager;
+import net.minecraft.client.texture.TextureUtil;
 import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
@@ -24,14 +23,12 @@ public class TextureImageRenderer implements ImageRenderer {
         this.location = location;
 
         final TextureManager manager = MinecraftClient.getInstance().getTextureManager();
-        //~ final Texture image = manager.getTexture(location);
-        final Object image = null;
+        final AbstractTexture image = manager.getTexture(location);
         if (image instanceof ImageTexture) {
             this.texture = (ImageTexture)image;
         } else {
-            //~ if (image != null && image.getGlId() != -1) {
-            if (image != null) { // XXX
-                //~ TextureUtil.releaseTextureId(image.getGlId());
+            if (image != null && image.getGlId() != -1) {
+                TextureUtil.releaseTextureId(image.getGlId());
             }
             this.texture = new ImageTexture(location);
             manager.registerTexture(location, texture);
@@ -81,7 +78,7 @@ public class TextureImageRenderer implements ImageRenderer {
             try (final InputStream is = resource.getInputStream()) {
                 final NativeImage bi = NativeImage.read(is);
 
-                //~ TextureUtil.prepareImage(this.getGlId(), bi.getWidth(), bi.getHeight());
+                TextureUtil.prepareImage(this.getGlId(), bi.getWidth(), bi.getHeight());
                 bi.upload(0, 0, 0, false);
 
                 width = bi.getWidth();
