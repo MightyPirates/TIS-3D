d3e52e9378010a99061df10ec11808187dc4571b
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-04-24 21:18:09 +0200

    ControllerBlock: Restore state label rendering

--- src/main/java/li/cil/tis3d/client/render/block/entity/ControllerBlockEntityRenderer.java
+++ src/main/java/li/cil/tis3d/client/render/block/entity/ControllerBlockEntityRenderer.java
@@ -1,10 +1,14 @@
 package li.cil.tis3d.client.render.block.entity;
 
 import li.cil.tis3d.common.block.entity.ControllerBlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.math.Matrix4f;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -12,6 +16,25 @@ import net.minecraft.util.hit.HitResult;
 public final class ControllerBlockEntityRenderer extends BlockEntityRenderer<ControllerBlockEntity> {
     public ControllerBlockEntityRenderer(BlockEntityRenderDispatcher dispatcher) {
         super(dispatcher);
+    }
+
+    private void renderState(final ControllerBlockEntity.ControllerState state, MatrixStack matrices, VertexConsumerProvider vc) {
+        final Camera camera = this.dispatcher.camera;
+        // I would add a check for distance to camera here, but this seems to happen automatically (?)
+        String str = I18n.translate(state.translateKey);
+        matrices.push();
+        matrices.translate(0.5f, 1.4f, 0.5f);
+        matrices.multiply(camera.getRotation());
+        matrices.scale(-0.025f, -0.025f, 0.025f);
+
+        final Matrix4f modMat = matrices.peek().getModel();
+        final int bg = MinecraftClient.getInstance().options.getTextBackgroundColor(0.25f);
+        final TextRenderer textRenderer = dispatcher.getTextRenderer();
+        float x = -textRenderer.getStringWidth(str) / 2.0f;
+        textRenderer.draw(str, x, 0, 0x20FFFFFF, false, modMat, vc, true, bg, 0xF000F0);
+        textRenderer.draw(str, x, 0, -1, false, modMat, vc, false, 0, 0xF000F0);
+
+        matrices.pop();
     }
 
     @Override
@@ -37,8 +60,6 @@ public final class ControllerBlockEntityRenderer extends BlockEntityRenderer<Con
             return;
         }
 
-        //~ disableLightmap(true);
-        //~ renderName(controller, I18n.translate(state.translateKey), x, y, z, 12);
-        //~ disableLightmap(false);
+        renderState(state, matrices, vertexConsumers);
     }
 }
