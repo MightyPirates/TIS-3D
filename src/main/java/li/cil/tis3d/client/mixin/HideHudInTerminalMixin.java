1658cadc9f4b79ec7a1af247fb267aaa3bd2c5ef
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-06-06 12:18:49 +0200

    HideHudInTerminalMixin: InGameHud#render takes a MatrixStack now

--- src/main/java/li/cil/tis3d/client/mixin/HideHudInTerminalMixin.java
+++ src/main/java/li/cil/tis3d/client/mixin/HideHudInTerminalMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public abstract class HideHudInTerminalMixin extends DrawableHelper {
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private void hideHud(final float tickDelta, final CallbackInfo ci) {
+    private void hideHud(final MatrixStack matrices, final float tickDelta, final CallbackInfo ci) {
         if (MinecraftClient.getInstance().currentScreen instanceof TerminalModuleGui) {
             ci.cancel();
         }
