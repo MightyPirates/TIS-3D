30177cfa8e21445c5cdc7c59952c74f0f88ccaf7
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-06-14 14:08:29 +0200

    WIP FontRendererUtils: Add translation helpers

--- src/main/java/li/cil/tis3d/util/FontRendererUtils.java
+++ src/main/java/li/cil/tis3d/util/FontRendererUtils.java
@@ -5,7 +5,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.StringRenderable;
 import net.minecraft.text.Text;
 
 import java.util.List;
@@ -27,13 +29,39 @@ public final class FontRendererUtils {
      */
     public static void addStringToTooltip(final String info, final List<Text> tooltip) {
         final MinecraftClient mc = MinecraftClient.getInstance();
-        //~ if (mc != null) {
-        if (false) { // XXX
-            final TextRenderer fontRenderer = mc.textRenderer;
-            //~ tooltip.addAll(fontRenderer.wrapLines(info, Constants.MAX_TOOLTIP_WIDTH).stream().map(LiteralText::new).collect(Collectors.toList()));
-        } else {
+        //if (mc != null) {
+            //final TextRenderer fontRenderer = mc.textRenderer;
+            //tooltip.addAll(fontRenderer.wrapLines(info, Constants.MAX_TOOLTIP_WIDTH).stream().map(LiteralText::new).collect(Collectors.toList()));
+        //} else {
             tooltip.add(new LiteralText(info));
+        //}
+    }
+
+    /**
+     * Tries to format the specified tooltip string so it does not exceed our maximum tooltip width
+     * and add it to the specified tooltip list. If no font renderer is available, add the info as is.
+     * The latter may happen when some other mod wants to get an items tooltip before the font renderer
+     * is available.
+     *
+     * @param info    the info to add to the tooltip.
+     * @param tooltip the tooltip to add the info to.
+     */
+    public static void addStringToTooltip2(final String info, final List<StringRenderable> tooltip) {
+        final MinecraftClient mc = MinecraftClient.getInstance();
+        if (mc != null) {
+            final TextRenderer fontRenderer = mc.textRenderer;
+            tooltip.addAll(fontRenderer.wrapLines(StringRenderable.plain(info), Constants.MAX_TOOLTIP_WIDTH).stream().collect(Collectors.toList()));
+        } else {
+            tooltip.add(StringRenderable.plain(info));
         }
+    }
+
+    public static void addStringListToTooltip(final List<String> infos, final List<StringRenderable> tooltip) {
+        tooltip.addAll(infos.stream().map(StringRenderable::plain).collect(Collectors.toList()));
+    }
+
+    public static StringRenderable translate(final String key) {
+        return StringRenderable.plain(I18n.translate(key));
     }
 
     // --------------------------------------------------------------------- //
