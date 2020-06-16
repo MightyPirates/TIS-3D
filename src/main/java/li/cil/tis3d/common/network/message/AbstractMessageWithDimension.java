8e8c1b3020f1caddc529903de2e6bfabd4411e8e
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-06-16 20:21:45 +0200

    Message/Handler: Rely on PacketContext-provided World instance
    1.16 completely overhauled how Dimension and DimensionType work.
    Just remove the DimensionType serialization for now and provide the
    associated World instance directly from Fabric's PacketContext.

--- src/main/java/li/cil/tis3d/common/network/message/AbstractMessageWithDimension.java
+++ src/main/java/li/cil/tis3d/common/network/message/AbstractMessageWithDimension.java
@@ -6,19 +6,11 @@ import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 
 public abstract class AbstractMessageWithDimension extends AbstractMessage {
-    private DimensionType dimension;
 
     AbstractMessageWithDimension(final World world) {
-        //~ this.dimension = world.dimension.getType();
     }
 
     AbstractMessageWithDimension() {
-    }
-
-    // --------------------------------------------------------------------- //
-
-    public DimensionType getDimension() {
-        return dimension;
     }
 
     // --------------------------------------------------------------------- //
@@ -26,13 +18,9 @@ public abstract class AbstractMessageWithDimension extends AbstractMessage {
 
     @Override
     public void fromBytes(final ByteBuf buf) {
-        final PacketByteBuf buffer = new PacketByteBuf(buf);
-        //~ dimension = DimensionType.byRawId(buffer.readVarInt());
     }
 
     @Override
     public void toBytes(final ByteBuf buf) {
-        final PacketByteBuf buffer = new PacketByteBuf(buf);
-        //~ buffer.writeVarInt(dimension.getRawId());
     }
 }
