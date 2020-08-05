e3d1d446429440831c5b4bd13ee29fde1a2adb6d
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-08-05 20:07:22 +0200

    API: Expose new font rendering

--- src/main/java/li/cil/tis3d/common/api/FontRendererAPIImpl.java
+++ src/main/java/li/cil/tis3d/common/api/FontRendererAPIImpl.java
@@ -1,9 +1,14 @@
 package li.cil.tis3d.common.api;
 
+import li.cil.tis3d.api.API;
 import li.cil.tis3d.api.detail.FontRendererAPI;
 import li.cil.tis3d.client.render.font.SmallFontRenderer;
+import li.cil.tis3d.client.render.font.NormalFontRenderer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 
 /**
  * Provide access to our tiny mono-space font.
@@ -18,6 +23,38 @@ public final class FontRendererAPIImpl implements FontRendererAPI {
     @Override
     public void drawString(final String string, final int maxChars) {
         SmallFontRenderer.INSTANCE.drawString(string, maxChars);
+    }
+
+    @Override
+    public void drawString(final API.Font font, final MatrixStack.Entry matrices, final VertexConsumer vc,
+                           final int light, final int overlay, final int color,
+                           final CharSequence value, final int maxChars) {
+        switch (font) {
+        case SmallFont:
+            SmallFontRenderer.INSTANCE.drawString(matrices, vc, light, overlay, color, value, maxChars);
+            break;
+
+        case NormalFont:
+            NormalFontRenderer.INSTANCE.drawString(matrices, vc, light, overlay, color, value, maxChars);
+            break;
+
+        default:
+            throw new RuntimeException("Font " + font + " not implemented");
+        }
+    }
+
+    @Override
+    public VertexConsumer chooseVertexConsumer(final API.Font font, final VertexConsumerProvider vcp) {
+        switch (font) {
+        case SmallFont:
+            return SmallFontRenderer.INSTANCE.chooseVertexConsumer(vcp);
+
+        case NormalFont:
+            return NormalFontRenderer.INSTANCE.chooseVertexConsumer(vcp);
+
+        default:
+            throw new RuntimeException("Font " + font + " not implemented");
+        }
     }
 
     @Override
