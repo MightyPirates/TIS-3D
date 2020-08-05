b21741b3f988d50b5955bd960fbc1d2bbc088094
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-08-05 20:07:22 +0200

    API: Expose new font rendering

--- src/main/java/li/cil/tis3d/api/detail/FontRendererAPI.java
+++ src/main/java/li/cil/tis3d/api/detail/FontRendererAPI.java
@@ -1,7 +1,11 @@
 package li.cil.tis3d.api.detail;
 
+import li.cil.tis3d.api.API;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 
 /**
  * API entry point for access to the tiny font renderer used on execution
@@ -12,6 +16,11 @@ import net.fabricmc.api.Environment;
  */
 @Environment(EnvType.CLIENT)
 public interface FontRendererAPI {
+    public enum Font {
+        SmallFont,
+        NormalFont
+    }
+
     /**
      * Render the specified string.
      *
@@ -29,6 +38,12 @@ public interface FontRendererAPI {
      * @param maxChars the maximum number of characters to render.
      */
     void drawString(final String string, final int maxChars);
+
+    void drawString(final Font font, final MatrixStack.Entry matrices, final VertexConsumer vc,
+                    final int light, final int overlay, final int color,
+                    final CharSequence value, final int maxChars);
+
+    VertexConsumer chooseVertexConsumer(final Font font, final VertexConsumerProvider vcp);
 
     /**
      * Get the width of the characters drawn with the font renderer, in pixels.
