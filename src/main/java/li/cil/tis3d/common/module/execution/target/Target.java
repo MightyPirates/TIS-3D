4500d145a6244d6a5c1fa1904eb8938ebae31c38
Florian Nücke <florian@nuecke.de>
2021-01-02 15:14:13 +0100

    Fixed broken Target to Port conversion introduced in c3e63a444fd633088f239ece0813c86e1a276f7f.

--- src/main/java/li/cil/tis3d/common/module/execution/target/Target.java
+++ src/main/java/li/cil/tis3d/common/module/execution/target/Target.java
@@ -23,7 +23,7 @@ public enum Target {
 
     public static final Set<Target> VALID_TARGETS = Arrays.stream(Target.values()).filter(t -> t != BAK).collect(Collectors.toSet());
 
-    public static final Port[] TO_PORT = new Port[]{Port.UP, Port.UP, Port.UP, Port.LEFT, Port.RIGHT, Port.UP, Port.DOWN, Port.UP, Port.UP};
+    public static final Port[] TO_PORT = new Port[]{Port.UP, Port.UP, Port.UP, Port.UP, Port.LEFT, Port.RIGHT, Port.UP, Port.DOWN, Port.UP, Port.UP};
 
     public static final Target[] FROM_PORT = new Target[]{Target.LEFT, Target.RIGHT, Target.UP, Target.DOWN};
 

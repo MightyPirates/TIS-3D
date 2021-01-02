MightyPirates/TIS-3D

commit c3e63a444fd633088f239ece0813c86e1a276f7f

Program counter register

src/main/java/li/cil/tis3d/common/module/execution/target/Target.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
  * Valid sources and sinks for move operations.
  */
 public enum Target {
+    PC,
     ACC,
     BAK,
     NIL,

src/main/java/li/cil/tis3d/common/module/execution/MachineImpl.java
@@ -30,6 +30,7 @@ public final class MachineImpl implements Machine {
         this.state = new MachineState();
         this.module = module;
         this.interfaces = ImmutableMap.<Target, TargetInterface>builder().
+            put(Target.PC, new PcTargetInterface(this)).
             put(Target.ACC, new AccTargetInterface(this)).
             put(Target.BAK, new BakTargetInterface(this)).
             put(Target.NIL, new NilTargetInterface(this)).

src/main/java/li/cil/tis3d/common/module/execution/target/PcTargetInterface.java
@@ -1,0 +1,55 @@
+package li.cil.tis3d.common.module.execution.target;
+
+import li.cil.tis3d.common.module.execution.Machine;
+
+/**
+ * Interface for the {@link Target#PC} target.
+ * <p>
+ * Provides instant read and write on the program counter
+ */
+public final class PcTargetInterface extends AbstractTargetInterface {
+    public PcTargetInterface(final Machine machine) {
+        super(machine);
+    }
+
+    // --------------------------------------------------------------------- //
+    // TargetInterface
+
+    @Override
+    public boolean beginWrite(final short value) {
+        getState().pc = value;
+        return true;
+    }
+
+    @Override
+    public boolean isWriting() {
+        return false;
+    }
+
+    @Override
+    public void beginRead() {
+    }
+
+    @Override
+    public boolean isReading() {
+        return false;
+    }
+
+    @Override
+    public boolean canTransfer() {
+        return true;
+    }
+
+    @Override
+    public short read() {
+        return (short) getState().pc;
+    }
+
+    // --------------------------------------------------------------------- //
+    // Object
+
+    @Override
+    public String toString() {
+        return "PC";
+    }
+}

commit 4500d145a6244d6a5c1fa1904eb8938ebae31c38

Fixed broken Target to Port conversion introduced in c3e63a444fd633088f239ece0813c86e1a276f7f.

src/main/java/li/cil/tis3d/common/module/execution/target/Target.java
@@ -23,7 +23,7 @@ public enum Target {
 
     public static final Set<Target> VALID_TARGETS = Arrays.stream(Target.values()).filter(t -> t != BAK).collect(Collectors.toSet());
 
-    public static final Port[] TO_PORT = new Port[]{Port.UP, Port.UP, Port.UP, Port.LEFT, Port.RIGHT, Port.UP, Port.DOWN, Port.UP, Port.UP};
+    public static final Port[] TO_PORT = new Port[]{Port.UP, Port.UP, Port.UP, Port.UP, Port.LEFT, Port.RIGHT, Port.UP, Port.DOWN, Port.UP, Port.UP};
 
     public static final Target[] FROM_PORT = new Target[]{Target.LEFT, Target.RIGHT, Target.UP, Target.DOWN};
 