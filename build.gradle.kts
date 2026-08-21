f3f5d4f652982ef57679779500a70cc4596dd05f
Florian Nücke <florian@nuecke.de>
2026-08-21 19:37:28 +0200

    Pulled out more common setup into functions

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,43 +56,9 @@ subprojects {
     }
 
     configureJava()
-
     configurePmd()
-
-    tasks.withType<Pmd>().configureEach {
-        exclude("**/mixin/**")
-        reports {
-            xml.required.set(false)
-            html.required.set(true)
-        }
-    }
-
-    tasks {
-        jar {
-            from(rootProject.file("LICENSE")) {
-                rename { "${it}_${modId}" }
-            }
-        }
-
-        withType<JavaCompile>().configureEach {
-            options.encoding = "utf-8"
-            options.release.set(21)
-            options.compilerArgs.addAll(
-                listOf(
-                    "-Xlint:all,-processing,-serial,-classfile,-this-escape",
-                    "-Xmaxwarns", "1000",
-                )
-            )
-        }
-    }
-
-    idea {
-        module {
-            for (exclude in arrayOf("out", "logs", "run")) {
-                excludeDirs.add(file(exclude))
-            }
-        }
-    }
+    embedLicenses()
+    configureIdeaExcludes()
 }
 
 val projectConfigurations = mapOf(
