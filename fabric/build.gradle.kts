da32c24e0429009481896c20a1d13f80060d2d48
Florian Nücke <florian@nuecke.de>
2026-09-25 12:44:34 +0200

    Fix gradle deprecation warnings

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -1,13 +1,13 @@
-val modId: String by project
+val modId = providers.gradleProperty("modId").get()
 val minecraftVersion: String = libs.versions.minecraft.get()
 val fabricApiVersion: String = libs.versions.fabric.api.get()
 val architecturyVersion: String = libs.versions.architectury.get()
 val forgeConfigPortVersion: String = libs.versions.fabric.forgeConfigPort.get()
 val manualVersion: String = markdownManualVersion(libs.versions.manual.get())
 
-val gameTestRuntime: Configuration by configurations.creating
+val gameTestRuntime = configurations.create("gameTestRuntime")
 val gameTestResultsDir = layout.buildDirectory.dir("test-results/gameTest")
-val devOnlyMods: Configuration by configurations.creating
+val devOnlyMods = configurations.create("devOnlyMods")
 val devOnlyModNames = provider { devOnlyMods.resolvedConfiguration.resolvedArtifacts.map { it.moduleVersion.id.name } }
 
 loom {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ pluginManagement {
 
 include("common")
 
-val enabledPlatforms: String by settings
+val enabledPlatforms = providers.gradleProperty("enabledPlatforms").get()
 for (enabledPlatform in enabledPlatforms.split(",")) {
     include(enabledPlatform)
 }
@@ -38,5 +38,5 @@ for (module in listOf("common") + enabledPlatforms.split(",")) {
     project(":gametest-$module").projectDir = file("gametest/$module")
 }
 
-val modId: String by settings
+val modId = providers.gradleProperty("modId").get()
 rootProject.name = modId
